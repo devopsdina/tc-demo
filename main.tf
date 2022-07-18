6eaad739f72f013561eafce0f5827df287e047ff
Dina Muscanell <dina@redhat.com>
2022-07-18 17:19:59 -0400

    adding provider and variables

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ terraform {
 
    required_version = ">=1.0"
 
-   required_providers {
-     azurerm = {
-       source = "hashicorp/azurerm"
-       version = "~>2.0"
-     }
-   }
+#    required_providers {
+#      azurerm = {
+#        source = "hashicorp/azurerm"
+#        version = "~>2.0"
+#      }
+#    }
  }
 
- provider "azurerm" {
-   features {}
- }
+#  provider "azurerm" {
+#    features {}
+#  }
 
  resource "azurerm_resource_group" "test" {
    name     = "tc-demo"
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "subscription to deploy resources to"
+}
+
+variable "ARM_CLIENT_ID" {
+  description = ""
+}
+
+variable "ARM_CLIENT_SECRET" {
+  description = ""
+}
+
+variable "ARM_TENANT_ID" {
+  description = ""
+}
