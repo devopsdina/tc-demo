3db06012b3da395c501cb2160d2dd93559377f5c
Dina Muscanell <dina@redhat.com>
2022-07-19 12:05:21 -0400

    updating vpcid and av region

--- aws/main.tf
+++ aws/main.tf
@@ -30,7 +30,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.app_vpc.id
   cidr_block        = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "public-subnet"
@@ -97,6 +97,7 @@ resource "aws_instance" "web" {
 # }
 resource "aws_security_group" "websg" {
   name = "web-sg01"
+  vpc_id = aws_vpc.app_vpc.id
   ingress {
     protocol = "tcp"
     from_port = 8080
